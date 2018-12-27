55d9c4b9644bd2c3593a13baf58eba1efe89cfb1
Daniel <tykang4182@gmail.com>
2018-12-27 10:11:52 +0800

    golang study on 26 Dec-2018

---
+++ switch.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	fmt.Println(time.Now())
+
+	i := 2
+	fmt.Println("write", i, "as")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's the weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatamI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I am a bool")
+		case int:
+			fmt.Println("I am an int")
+		default:
+			fmt.Printf("Do not know type %T\n", t)
+		}
+	}
+
+	whatamI(true)
+	whatamI(1)
+	whatamI("hey")
+}

5836b3c5914e6826c2a890aa31d684396507350d
Daniel <tykang4182@gmail.com>
2019-01-04 10:56:56 +0800

    golang study 04-Jan-2019

---
+++ Multiple Return Values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c, _ := vals()
+	fmt.Println(c)
+
+}
